0f6139f16d4dbbf40e94e052628ae33ea19d9384
Kfir Nissim <kfirr99.gmail.com>
2020-03-28 17:32:52 +0300

    add countdown component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountdownComponent } from './countdown/countdown.component';
+import { FlipclockComponent } from './countdown/flipclock/flipclock.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountdownComponent,
+    FlipclockComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/countdown/flipclock/flipclock.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flipclock',
+  templateUrl: './flipclock.component.html',
+  styleUrls: ['./flipclock.component.scss']
+})
+export class FlipclockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
